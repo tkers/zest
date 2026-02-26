3594750f0dba502a313d74ccf8c9079d7eb38aa6
Tijn Kersjes <tijn@divbyzero.nl>
2026-02-26 14:14:02 +0100

    Wrap ZestAudio in IIFE

--- core/audio.js
+++ core/audio.js
@@ -1,314 +1,318 @@
-const FREQ = [
-  16.351597831287414, 17.323914436054505, 18.354047994837977,
-  19.445436482630054, 20.601722307054366, 21.826764464562746,
-  23.124651419477154, 24.499714748859326, 25.956543598746574, 27.5,
-  29.13523509488062, 30.86770632850775, 32.70319566257483, 34.64782887210902,
-  36.70809598967594, 38.89087296526011, 41.20344461410874, 43.653528929125486,
-  46.24930283895431, 48.999429497718666, 51.91308719749314, 55,
-  58.27047018976124, 61.7354126570155, 65.40639132514966, 69.29565774421803,
-  73.41619197935188, 77.78174593052022, 82.40688922821748, 87.30705785825097,
-  92.49860567790861, 97.99885899543733, 103.82617439498628, 110,
-  116.54094037952248, 123.47082531403103, 130.8127826502993, 138.59131548843604,
-  146.8323839587038, 155.56349186104043, 164.81377845643496, 174.61411571650194,
-  184.99721135581723, 195.99771799087463, 207.65234878997256, 220,
-  233.08188075904496, 246.94165062806206, 261.6255653005986, 277.1826309768721,
-  293.6647679174076, 311.12698372208087, 329.6275569128699, 349.2282314330039,
-  369.99442271163446, 391.99543598174927, 415.3046975799451, 440,
-  466.1637615180899, 493.8833012561241, 523.2511306011972, 554.3652619537442,
-  587.3295358348151, 622.2539674441618, 659.2551138257398, 698.4564628660078,
-  739.9888454232689, 783.9908719634986, 830.6093951598903, 880,
-  932.3275230361799, 987.7666025122483, 1046.5022612023945, 1108.7305239074883,
-  1174.6590716696303, 1244.5079348883235, 1318.5102276514797,
-  1396.9129257320155, 1479.9776908465378, 1567.981743926997, 1661.2187903197805,
-  1760, 1864.6550460723597, 1975.533205024496, 2093.004522404789,
-  2217.461047814977, 2349.31814333926, 2489.015869776647, 2637.0204553029594,
-  2793.825851464031, 2959.9553816930757, 3135.9634878539946, 3322.437580639561,
-  3520, 3729.3100921447194, 3951.066410048992, 4186.009044809578,
-  4434.922095629954, 4698.63628667852, 4978.031739553294, 5274.040910605919,
-  5587.651702928062, 5919.910763386151, 6271.926975707989, 6644.875161279122,
-  7040, 7458.620184289437, 7902.132820097988,
-]
-const freqForNote = (note, oct) => FREQ[note - 1 + oct * 12]
+ZestAudio = (function () {
+  const FREQ = [
+    16.351597831287414, 17.323914436054505, 18.354047994837977,
+    19.445436482630054, 20.601722307054366, 21.826764464562746,
+    23.124651419477154, 24.499714748859326, 25.956543598746574, 27.5,
+    29.13523509488062, 30.86770632850775, 32.70319566257483, 34.64782887210902,
+    36.70809598967594, 38.89087296526011, 41.20344461410874, 43.653528929125486,
+    46.24930283895431, 48.999429497718666, 51.91308719749314, 55,
+    58.27047018976124, 61.7354126570155, 65.40639132514966, 69.29565774421803,
+    73.41619197935188, 77.78174593052022, 82.40688922821748, 87.30705785825097,
+    92.49860567790861, 97.99885899543733, 103.82617439498628, 110,
+    116.54094037952248, 123.47082531403103, 130.8127826502993,
+    138.59131548843604, 146.8323839587038, 155.56349186104043,
+    164.81377845643496, 174.61411571650194, 184.99721135581723,
+    195.99771799087463, 207.65234878997256, 220, 233.08188075904496,
+    246.94165062806206, 261.6255653005986, 277.1826309768721, 293.6647679174076,
+    311.12698372208087, 329.6275569128699, 349.2282314330039,
+    369.99442271163446, 391.99543598174927, 415.3046975799451, 440,
+    466.1637615180899, 493.8833012561241, 523.2511306011972, 554.3652619537442,
+    587.3295358348151, 622.2539674441618, 659.2551138257398, 698.4564628660078,
+    739.9888454232689, 783.9908719634986, 830.6093951598903, 880,
+    932.3275230361799, 987.7666025122483, 1046.5022612023945,
+    1108.7305239074883, 1174.6590716696303, 1244.5079348883235,
+    1318.5102276514797, 1396.9129257320155, 1479.9776908465378,
+    1567.981743926997, 1661.2187903197805, 1760, 1864.6550460723597,
+    1975.533205024496, 2093.004522404789, 2217.461047814977, 2349.31814333926,
+    2489.015869776647, 2637.0204553029594, 2793.825851464031,
+    2959.9553816930757, 3135.9634878539946, 3322.437580639561, 3520,
+    3729.3100921447194, 3951.066410048992, 4186.009044809578, 4434.922095629954,
+    4698.63628667852, 4978.031739553294, 5274.040910605919, 5587.651702928062,
+    5919.910763386151, 6271.926975707989, 6644.875161279122, 7040,
+    7458.620184289437, 7902.132820097988,
+  ]
+  const freqForNote = (note, oct) => FREQ[note - 1 + oct * 12]
 
-const VOICE_TYPES = ['sine', 'square', 'sawtooth', 'triangle', 'noise']
-const parseVoiceType = (ix) => VOICE_TYPES[ix]
+  const VOICE_TYPES = ['sine', 'square', 'sawtooth', 'triangle', 'noise']
+  const parseVoiceType = (ix) => VOICE_TYPES[ix]
 
-const DEFAULT_ENVELOPE = {
-  attack: 0.005,
-  decay: 0.1,
-  sustain: 0.5,
-  release: 0.1,
-}
-
-let audioCtx
-const enableAudio = () => {
-  if (!audioCtx) {
-    audioCtx = new AudioContext()
-    console.log(`[ZestAudio] Created AudioContext (${audioCtx.state})`)
+  const DEFAULT_ENVELOPE = {
+    attack: 0.005,
+    decay: 0.1,
+    sustain: 0.5,
+    release: 0.1,
   }
-  if (audioCtx.state === 'suspended') {
-    console.log('[ZestAudio] Resuming AudioContext...')
-    audioCtx.resume().then(() => {
-      console.log('[ZestAudio] Active!')
-    })
+
+  let audioCtx
+  const enable = () => {
+    if (!audioCtx) {
+      audioCtx = new AudioContext()
+      console.log(`[ZestAudio] Created AudioContext (${audioCtx.state})`)
+    }
+    if (audioCtx.state === 'suspended') {
+      console.log('[ZestAudio] Resuming AudioContext...')
+      audioCtx.resume().then(() => {
+        console.log('[ZestAudio] Active!')
+      })
+    }
   }
-}
 
-let audioIntervalWorker
-const onAudioTick = (cb) => {
-  if (!audioIntervalWorker) {
-    const audioIntervalBlob = new Blob(
-      [`setInterval(() => postMessage('tick'), 50)`], // 1000ms / 20 FPS
-      { type: 'text/javascript' }
-    )
-    audioIntervalWorker = new Worker(URL.createObjectURL(audioIntervalBlob))
-  }
-  audioIntervalWorker.onmessage = cb
-}
-const stopAudioTicker = () => {
-  if (!audioIntervalWorker) return
-  audioIntervalWorker.onmessage = null
-  audioIntervalWorker.terminate()
-  audioIntervalWorker = null
-}
-
-const createNoiseOscillator = () => {
-  let isPlaying = false
-  let freq = 0
-  let current = 0
-  let left = 0
-
-  const buzzNode = audioCtx.createScriptProcessor(0, 1, 1)
-  const noise = {
-    connect: (n) => buzzNode.connect(n),
-    start: () => {
-      isPlaying = true
-    },
-    stop: (t) => {
-      setTimeout(
-        () => {
-          isPlaying = false
-          buzzNode.disconnect()
-        },
-        1000 * (t - audioCtx.currentTime)
+  let audioIntervalWorker
+  const onAudioTick = (cb) => {
+    if (!audioIntervalWorker) {
+      const audioIntervalBlob = new Blob(
+        [`setInterval(() => postMessage('tick'), 50)`], // 1000ms / 20 FPS
+        { type: 'text/javascript' }
       )
-    },
-    frequency: {
-      setValueAtTime: (f) => {
-        freq = f
+      audioIntervalWorker = new Worker(URL.createObjectURL(audioIntervalBlob))
+    }
+    audioIntervalWorker.onmessage = cb
+  }
+  const stopAudioTicker = () => {
+    if (!audioIntervalWorker) return
+    audioIntervalWorker.onmessage = null
+    audioIntervalWorker.terminate()
+    audioIntervalWorker = null
+  }
+
+  const createNoiseOscillator = () => {
+    let isPlaying = false
+    let freq = 0
+    let current = 0
+    let left = 0
+
+    const buzzNode = audioCtx.createScriptProcessor(0, 1, 1)
+    const noise = {
+      connect: (n) => buzzNode.connect(n),
+      start: () => {
+        isPlaying = true
       },
-    },
-  }
-
-  buzzNode.onaudioprocess = (e) => {
-    const out = e.outputBuffer.getChannelData(0)
-    if (!isPlaying) {
-      out.fill(0)
-      return
+      stop: (t) => {
+        setTimeout(
+          () => {
+            isPlaying = false
+            buzzNode.disconnect()
+          },
+          1000 * (t - audioCtx.currentTime)
+        )
+      },
+      frequency: {
+        setValueAtTime: (f) => {
+          freq = f
+        },
+      },
     }
 
-    const length = audioCtx.sampleRate / 16 / freq
-    for (let i = 0; i < out.length; i++) {
-      if (left <= 0) {
-        current = Math.random() - 0.5
-        left = length
-      }
-      out[i] = current
-      left--
-    }
-  }
-
-  return noise
-}
-
-const createVoice = (typeIx, envelope = {}) => {
-  const volume = envelope?.volume ?? 1
-  const attack = envelope?.attack ?? DEFAULT_ENVELOPE.attack
-  const decay = envelope?.decay ?? DEFAULT_ENVELOPE.decay
-  const sustain = envelope?.sustain ?? DEFAULT_ENVELOPE.sustain
-  const release = envelope?.release ?? DEFAULT_ENVELOPE.release
-
-  let oscNode, gainNode
-  const playNote = (note, oct, hold, time) => {
-    if (!audioCtx) return
-    const tone = freqForNote(note, oct)
-
-    if (typeIx < 4) {
-      oscNode = audioCtx.createOscillator()
-      oscNode.type = VOICE_TYPES[typeIx]
-    } else {
-      oscNode = createNoiseOscillator()
-    }
-
-    gainNode = audioCtx.createGain()
-    gainNode.gain.value = 0
-    oscNode.connect(gainNode)
-    gainNode.connect(audioCtx.destination)
-
-    const t0 = time ?? audioCtx.currentTime
-    let ta = t0 + attack
-    let td = ta + decay
-    const ts = t0 + hold
-    const tr = ts + release
-
-    let vmax = volume
-    let vsus = vmax * sustain
-
-    if (hold < attack) {
-      ta = ts
-      vmax *= hold / attack
-      td = -1
-      vsus = vmax
-    } else if (hold < attack + decay) {
-      td = ts - attack
-      vsus = ((hold - attack) / decay) * (vsus - vmax) + vmax
-    }
-
-    oscNode.start()
-
-    oscNode.frequency.setValueAtTime(tone, t0)
-    gainNode.gain.setValueAtTime(0, t0)
-    gainNode.gain.linearRampToValueAtTime(vmax, ta)
-    if (td > 0) {
-      gainNode.gain.linearRampToValueAtTime(vsus, td)
-      gainNode.gain.setValueAtTime(vsus, td)
-    }
-    gainNode.gain.setValueAtTime(vsus, ts)
-    gainNode.gain.linearRampToValueAtTime(0, tr)
-    oscNode.stop(tr)
-  }
-
-  const stop = () => {
-    gainNode?.gain.cancelScheduledValues(0)
-    gainNode?.gain.setValueAtTime(0, 0)
-    oscNode?.stop()
-  }
-
-  return { playNote, stop }
-}
-
-const playSound = (sound) => {
-  const voice = createVoice(sound.type, sound.envelope)
-
-  const BPM = sound.bpm ?? 120
-  const tickLength = 15 / BPM // (1 / 4) * (60 / BPM)
-  let pos = -1
-  let now = audioCtx.currentTime
-
-  const nextTick = () => {
-    while (now < audioCtx.currentTime + SCHEDULE_WINDOW) {
-      pos++
-      now += tickLength
-
-      if (pos >= sound.ticks) {
+    buzzNode.onaudioprocess = (e) => {
+      const out = e.outputBuffer.getChannelData(0)
+      if (!isPlaying) {
+        out.fill(0)
         return
       }
 
-      const note = sound.notes[pos * 3]
-      const oct = sound.notes[pos * 3 + 1]
-      const hold = sound.notes[pos * 3 + 2]
-
-      if (note > 0) {
-        voice.playNote(note, oct, hold * tickLength, now)
+      const length = audioCtx.sampleRate / 16 / freq
+      for (let i = 0; i < out.length; i++) {
+        if (left <= 0) {
+          current = Math.random() - 0.5
+          left = length
+        }
+        out[i] = current
+        left--
       }
     }
 
-    setTimeout(nextTick, SCHEDULE_INTERVAL)
+    return noise
   }
 
-  nextTick()
-}
+  const createVoice = (typeIx, envelope = {}) => {
+    const volume = envelope?.volume ?? 1
+    const attack = envelope?.attack ?? DEFAULT_ENVELOPE.attack
+    const decay = envelope?.decay ?? DEFAULT_ENVELOPE.decay
+    const sustain = envelope?.sustain ?? DEFAULT_ENVELOPE.sustain
+    const release = envelope?.release ?? DEFAULT_ENVELOPE.release
 
-const stopSong = () => {
-  if (!ZestAudio.__currentSong) return
-  ZestAudio.__currentSong.stop = true
-  delete ZestAudio.__currentSong
-}
+    let oscNode, gainNode
+    const playNote = (note, oct, hold, time) => {
+      if (!audioCtx) return
+      const tone = freqForNote(note, oct)
 
-const pauseSong = () => {
-  if (!ZestAudio.__currentSong) return
-  ZestAudio.__currentSong.pause = true
-}
-
-const resumeSong = () => {
-  if (!ZestAudio.__currentSong) return
-  ZestAudio.__currentSong.pause = false
-}
-
-const setTempo = (bpm) => {
-  if (!ZestAudio.__currentSong) return
-  ZestAudio.__currentSong.tickLength = 15 / bpm // (1 / 4) * (60 / bpm)
-}
-
-const SCHEDULE_INTERVAL = 50 // 20 FPS (1000ms / 20)
-const SCHEDULE_WINDOW = 0.1 // 100ms ahead, should be >interval
-
-const playSong = (song, loop, onEnd) => {
-  const voices = [
-    createVoice(0, song.voices?.[0]),
-    createVoice(1, song.voices?.[1]),
-    createVoice(2, song.voices?.[2]),
-    createVoice(3, song.voices?.[3]),
-    createVoice(4, song.voices?.[4]),
-  ]
-
-  const BPM = song.bpm ?? 120
-  const tickLength = 15 / BPM // (1 / 4) * (60 / BPM)
-  let pos = -1
-  let now = audioCtx.currentTime
-
-  stopSong()
-  const signal = { stop: false, pause: false, tickLength }
-  ZestAudio.__currentSong = signal
-
-  const nextTick = () => {
-    if (signal.pause) {
-      now = audioCtx.currentTime
-      return
-    }
-
-    if (signal.stop) {
-      for (let v = 0; v < voices.length; v++) {
-        voices[v].stop()
+      if (typeIx < 4) {
+        oscNode = audioCtx.createOscillator()
+        oscNode.type = VOICE_TYPES[typeIx]
+      } else {
+        oscNode = createNoiseOscillator()
       }
-      stopAudioTicker()
-      return
+
+      gainNode = audioCtx.createGain()
+      gainNode.gain.value = 0
+      oscNode.connect(gainNode)
+      gainNode.connect(audioCtx.destination)
+
+      const t0 = time ?? audioCtx.currentTime
+      let ta = t0 + attack
+      let td = ta + decay
+      const ts = t0 + hold
+      const tr = ts + release
+
+      let vmax = volume
+      let vsus = vmax * sustain
+
+      if (hold < attack) {
+        ta = ts
+        vmax *= hold / attack
+        td = -1
+        vsus = vmax
+      } else if (hold < attack + decay) {
+        td = ts - attack
+        vsus = ((hold - attack) / decay) * (vsus - vmax) + vmax
+      }
+
+      oscNode.start()
+
+      oscNode.frequency.setValueAtTime(tone, t0)
+      gainNode.gain.setValueAtTime(0, t0)
+      gainNode.gain.linearRampToValueAtTime(vmax, ta)
+      if (td > 0) {
+        gainNode.gain.linearRampToValueAtTime(vsus, td)
+        gainNode.gain.setValueAtTime(vsus, td)
+      }
+      gainNode.gain.setValueAtTime(vsus, ts)
+      gainNode.gain.linearRampToValueAtTime(0, tr)
+      oscNode.stop(tr)
     }
 
-    while (now < audioCtx.currentTime + SCHEDULE_WINDOW) {
-      pos++
-      now += signal.tickLength
+    const stop = () => {
+      gainNode?.gain.cancelScheduledValues(0)
+      gainNode?.gain.setValueAtTime(0, 0)
+      oscNode?.stop()
+    }
 
-      if (pos >= song.ticks) {
-        if (loop) {
-          pos = song.loopFrom ?? 0
-        } else {
-          stopAudioTicker()
-          return onEnd && onEnd()
+    return { playNote, stop }
+  }
+
+  const playSound = (sound) => {
+    const voice = createVoice(sound.type, sound.envelope)
+
+    const BPM = sound.bpm ?? 120
+    const tickLength = 15 / BPM // (1 / 4) * (60 / BPM)
+    let pos = -1
+    let now = audioCtx.currentTime
+
+    const nextTick = () => {
+      while (now < audioCtx.currentTime + SCHEDULE_WINDOW) {
+        pos++
+        now += tickLength
+
+        if (pos >= sound.ticks) {
+          return
         }
-      }
 
-      for (let v = 0; v < voices.length; v++) {
-        const note = song.notes[v][pos * 3]
-        const oct = song.notes[v][pos * 3 + 1]
-        const hold = song.notes[v][pos * 3 + 2]
+        const note = sound.notes[pos * 3]
+        const oct = sound.notes[pos * 3 + 1]
+        const hold = sound.notes[pos * 3 + 2]
 
         if (note > 0) {
-          voices[v].playNote(note, oct, hold * signal.tickLength, now)
+          voice.playNote(note, oct, hold * tickLength, now)
+        }
+      }
+
+      setTimeout(nextTick, SCHEDULE_INTERVAL)
+    }
+
+    nextTick()
+  }
+
+  const stopSong = () => {
+    if (!ZestAudio.__currentSong) return
+    ZestAudio.__currentSong.stop = true
+    delete ZestAudio.__currentSong
+  }
+
+  const pauseSong = () => {
+    if (!ZestAudio.__currentSong) return
+    ZestAudio.__currentSong.pause = true
+  }
+
+  const resumeSong = () => {
+    if (!ZestAudio.__currentSong) return
+    ZestAudio.__currentSong.pause = false
+  }
+
+  const setTempo = (bpm) => {
+    if (!ZestAudio.__currentSong) return
+    ZestAudio.__currentSong.tickLength = 15 / bpm // (1 / 4) * (60 / bpm)
+  }
+
+  const SCHEDULE_INTERVAL = 50 // 20 FPS (1000ms / 20)
+  const SCHEDULE_WINDOW = 0.1 // 100ms ahead, should be >interval
+
+  const playSong = (song, loop, onEnd) => {
+    const voices = [
+      createVoice(0, song.voices?.[0]),
+      createVoice(1, song.voices?.[1]),
+      createVoice(2, song.voices?.[2]),
+      createVoice(3, song.voices?.[3]),
+      createVoice(4, song.voices?.[4]),
+    ]
+
+    const BPM = song.bpm ?? 120
+    const tickLength = 15 / BPM // (1 / 4) * (60 / BPM)
+    let pos = -1
+    let now = audioCtx.currentTime
+
+    stopSong()
+    const signal = { stop: false, pause: false, tickLength }
+    ZestAudio.__currentSong = signal
+
+    const nextTick = () => {
+      if (signal.pause) {
+        now = audioCtx.currentTime
+        return
+      }
+
+      if (signal.stop) {
+        for (let v = 0; v < voices.length; v++) {
+          voices[v].stop()
+        }
+        stopAudioTicker()
+        return
+      }
+
+      while (now < audioCtx.currentTime + SCHEDULE_WINDOW) {
+        pos++
+        now += signal.tickLength
+
+        if (pos >= song.ticks) {
+          if (loop) {
+            pos = song.loopFrom ?? 0
+          } else {
+            stopAudioTicker()
+            return onEnd && onEnd()
+          }
+        }
+
+        for (let v = 0; v < voices.length; v++) {
+          const note = song.notes[v][pos * 3]
+          const oct = song.notes[v][pos * 3 + 1]
+          const hold = song.notes[v][pos * 3 + 2]
+
+          if (note > 0) {
+            voices[v].playNote(note, oct, hold * signal.tickLength, now)
+          }
         }
       }
     }
+
+    onAudioTick(nextTick)
   }
 
-  onAudioTick(nextTick)
-}
-
-ZestAudio = {
-  enable: enableAudio,
-  playSound,
-  playSong,
-  stopSong,
-  pauseSong,
-  resumeSong,
-  setTempo,
-}
+  return {
+    enable,
+    playSound,
+    playSong,
+    stopSong,
+    pauseSong,
+    resumeSong,
+    setTempo,
+  }
+})()
