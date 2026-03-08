b198c9d9faf4a1c42f9d486fe83f371300755c08
Tijn Kersjes <tijn@divbyzero.nl>
2026-03-08 17:32:41 +0100

    Wrap utils in iife

--- core/utils.js
+++ core/utils.js
@@ -1,11 +1,14 @@
-const deepCopy = (x) => JSON.parse(JSON.stringify(x))
+'use strict'
+;(function () {
+  const deepCopy = (x) => JSON.parse(JSON.stringify(x))
 
-Zest.minify = function (original) {
-  const gameData = deepCopy(original)
-  delete gameData.editor
-  gameData.scripts.forEach((script) => {
-    delete script?.data?.__comments
-    delete script?.data?.__srcOrder
-  })
-  return gameData
-}
+  Zest.minify = function (original) {
+    const gameData = deepCopy(original)
+    delete gameData.editor
+    gameData.scripts.forEach((script) => {
+      delete script?.data?.__comments
+      delete script?.data?.__srcOrder
+    })
+    return gameData
+  }
+})()
