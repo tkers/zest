7cee6a38c7159430634becfbdd0807fb320c1800
Tijn Kersjes <tijn@divbyzero.nl>
2026-03-21 21:38:59 +0100

    Force integer scaling for canvas

--- assets/js/embed.js
+++ assets/js/embed.js
@@ -3,6 +3,19 @@ window.addEventListener('load', () => {
   const lcd = document.getElementById('lcd')
   const autoplay = main.className == 'clicked'
 
+  // integer scale the canvas
+  const w = lcd.width
+  const h = lcd.height
+  function lcdScale(canvas) {
+    const wScale = Math.floor(window.innerWidth / w)
+    const hScale = Math.floor(window.innerHeight / h)
+    const scale = Math.max(1, Math.min(wScale, hScale))
+    lcd.style.width = `${scale * w}px`
+    lcd.style.height = `${scale * h}px`
+  }
+  window.addEventListener('resize', lcdScale)
+  lcdScale()
+
   // load and start the game
   Zest.prototype.log = null
   const game = autoplay ? Zest.run(gameData, lcd) : Zest.load(gameData, lcd)
@@ -10,9 +23,10 @@ window.addEventListener('load', () => {
     ZestAudio.enable()
   })
 
+  main.style.backgroundColor = game.config.colorBlack
   game.addEventListener('config', (e) => {
     if (e.detail.key === 'colorBlack') {
-      lcd.style.backgroundColor = e.detail.value
+      main.style.backgroundColor = e.detail.value
     }
   })
 
--- bundler_template/main.js
+++ bundler_template/main.js
@@ -3,6 +3,19 @@ window.addEventListener('load', () => {
   const lcd = document.getElementById('lcd')
   const autoplay = main.className == 'clicked'
 
+  // integer scale the canvas
+  const w = lcd.width
+  const h = lcd.height
+  function lcdScale(canvas) {
+    const wScale = Math.floor(window.innerWidth / w)
+    const hScale = Math.floor(window.innerHeight / h)
+    const scale = Math.max(1, Math.min(wScale, hScale))
+    lcd.style.width = `${scale * w}px`
+    lcd.style.height = `${scale * h}px`
+  }
+  window.addEventListener('resize', lcdScale)
+  lcdScale()
+
   // load and start the game
   Zest.prototype.log = null
   const game = autoplay ? Zest.run(gameData, lcd) : Zest.load(gameData, lcd)
@@ -11,10 +24,10 @@ window.addEventListener('load', () => {
   })
 
   // config on LOAD event does not correctly trigger event
-  lcd.style.backgroundColor = game.config.colorBlack
+  main.style.backgroundColor = game.config.colorBlack
   game.addEventListener('config', (e) => {
     if (e.detail.key === 'colorBlack') {
-      lcd.style.backgroundColor = e.detail.value
+      main.style.backgroundColor = e.detail.value
     }
   })
 
